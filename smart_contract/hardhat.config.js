51d763164075175567ff51ac283385c0d622ba7b
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-04-15 18:15:07 +0530

    deploy file

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,11 @@
-// link - 
-
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/hFppMLWov1yNyJwu6yWJEcBwXfEY46HF',
-      accounts: ['dc75b9df63ca87af2edc20251b7eb0a416606e2b70df56592bb8d3021fb7a7a2']  
-    }
-  }
-}
+      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
+      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+    },
+  },
+};
